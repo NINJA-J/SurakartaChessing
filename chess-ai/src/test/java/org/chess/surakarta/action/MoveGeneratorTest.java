eff084d7688d42bfd13e8284eab45d7a730a273a
tiyan <tianrun.yan@nokia.com>
2023-10-10 12:05:31 -0700

    Update test

--- chess-ai/src/test/java/org/chess/surakarta/action/MoveGeneratorTest.java
+++ chess-ai/src/test/java/org/chess/surakarta/action/MoveGeneratorTest.java
@@ -4,13 +4,9 @@ import org.chess.surakarta.chessai.action.MoveGenerator;
 import org.chess.surakarta.chessai.entity.Board;
 import org.chess.surakarta.chessai.entity.Move;
 import org.junit.Assert;
+import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-
-import static org.junit.Assert.*;
 
 public class MoveGeneratorTest {
 
@@ -24,7 +20,7 @@ public class MoveGeneratorTest {
     public void tearDown() throws Exception {
     }
 
-    @org.junit.Test
+    @Test
     public void availableMove() {
         List<Move> moves = generator.availableMove(new Board(new int[][] {
                 {0, 0, 0, 0, 2, 0},
@@ -34,12 +30,32 @@ public class MoveGeneratorTest {
                 {0, 0, 1, 2, 0, 2},
                 {2, 2, 2, 2, 0, 2}
         }, true));
-        Assert.assertEquals(moves.get(0), Move.as(1, 4, 0, 4, true));
-        Assert.assertEquals(moves.get(1), Move.as(3, 4, 4, 5, true));
-        Assert.assertEquals(moves.get(2), Move.as(4, 2, 5, 1, true));
-        Assert.assertEquals(moves.get(3), Move.as(1, 1, 5, 1, true));
-        Assert.assertEquals(moves.get(4), Move.as(3, 4, 5, 3, true));
-        Assert.assertEquals(moves.get(5), Move.as(3, 4, 5, 2, true));
+        Assert.assertEquals(moves.get(0), Move.arc(1, 4, 0, 4, true));
+        Assert.assertEquals(moves.get(1), Move.arc(3, 4, 4, 5, true));
+        Assert.assertEquals(moves.get(2), Move.arc(4, 2, 5, 1, true));
+        Assert.assertEquals(moves.get(3), Move.arc(1, 1, 5, 1, true));
+        Assert.assertEquals(moves.get(4), Move.arc(3, 4, 5, 3, true));
+        Assert.assertEquals(moves.get(5), Move.arc(3, 4, 5, 2, true));
         Assert.assertFalse(moves.get(6).isArc());
+    }
+
+    @Test
+    public void availableMove2() {
+        List<Move> moves = generator.availableMove(new Board(new int[][] {
+                {0, 2, 0, 2, 2, 2},
+                {2, 2, 2, 2, 2, 2},
+                {2, 2, 0, 0, 0, 0},
+                {0, 0, 1, 1, 1, 1},
+                {1, 1, 0, 1, 1, 0},
+                {1, 1, 0, 1, 1, 0}
+        }, false));
+        System.out.println(moves);
+//        Assert.assertEquals(moves.get(0), Move.as(1, 4, 0, 4, true));
+//        Assert.assertEquals(moves.get(1), Move.as(3, 4, 4, 5, true));
+//        Assert.assertEquals(moves.get(2), Move.as(4, 2, 5, 1, true));
+//        Assert.assertEquals(moves.get(3), Move.as(1, 1, 5, 1, true));
+//        Assert.assertEquals(moves.get(4), Move.as(3, 4, 5, 3, true));
+//        Assert.assertEquals(moves.get(5), Move.as(3, 4, 5, 2, true));
+//        Assert.assertFalse(moves.get(6).isArc());
     }
 }
